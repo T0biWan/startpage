35480687ca5e983b7ec5cdae64afb9455ad119fa
t0biwan <tobi.klatt@web.de>
2018-04-03 15:02:18 +0200

    conditional rendering with now three engines

--- src/components/searchbar.vue
+++ src/components/searchbar.vue
@@ -1,41 +1,109 @@
 <template>
   <section class="section">
-    <!-- https://dees.dict.cc/?s=test -->
-    <!-- https://dees.dict.cc/?s=test -->
-    <!-- http://www.google.com/search -->
-    <form method="get" action="http://www.google.com/search">
-      <div class="field has-addons">
-        <div class="control has-icons-left">
-          <span class="select">
-            <select v-model="selected_search_engine">
-              <option>google</option>
-              <option>duckduckgo</option>
-              <option>dict.cc</option>
-              <option>dees.cc</option>
-              <option>google images</option>
-              <option>wikipedia.de</option>
-              <option>wikipedia.com</option>
-              <option>Metal Archive</option>
-            </select>
-          </span>
-          <span class="icon is-small is-left">
-            <i class="fab fa-searchengin"></i>
-          </span>
+
+    <template v-if="selected_search_engine == 'google'">
+      <form method="get" action="http://www.google.com/search">
+        <div class="field has-addons">
+          <div class="control has-icons-left">
+            <span class="select">
+              <select v-model="selected_search_engine">
+                <option>google</option>
+                <option>duckduckgo</option>
+                <option>dict.cc</option>
+                <option>dees.cc</option>
+                <option>google images</option>
+                <option>wikipedia.de</option>
+                <option>wikipedia.com</option>
+                <option>Metal Archive</option>
+              </select>
+            </span>
+            <span class="icon is-small is-left">
+              <i class="fab fa-searchengin"></i>
+            </span>
+          </div>
+          <div class="control is-expanded">
+            <input class="input" type="text" placeholder="search..." name="q" maxlength="255">
+          </div>
+          <div class="control">
+            <button type="submit" class="button is-info"><i class="fas fa-search"></i></button>
+          </div>
         </div>
-        <div class="control is-expanded">
-          <input class="input" type="text" placeholder="search..." name="query" maxlength="255">
+      </form>
+    </template>
+
+    <template v-else-if="selected_search_engine == 'dict.cc'">
+      <form method="get" action="https://dict.cc/">
+        <div class="field has-addons">
+          <div class="control has-icons-left">
+            <span class="select">
+              <select v-model="selected_search_engine">
+                <option>google</option>
+                <option>duckduckgo</option>
+                <option>dict.cc</option>
+                <option>dees.cc</option>
+                <option>google images</option>
+                <option>wikipedia.de</option>
+                <option>wikipedia.com</option>
+                <option>Metal Archive</option>
+              </select>
+            </span>
+            <span class="icon is-small is-left">
+              <i class="fab fa-searchengin"></i>
+            </span>
+          </div>
+          <div class="control is-expanded">
+            <input class="input" type="text" placeholder="search..." name="s" maxlength="255">
+          </div>
+          <div class="control">
+            <button type="submit" class="button is-info"><i class="fas fa-search"></i></button>
+          </div>
         </div>
-        <div class="control">
-          <button type="submit" class="button is-info"><i class="fas fa-search"></i></button>
+      </form>
+    </template>
+
+    <template v-else-if="selected_search_engine == 'dees.cc'">
+      <form method="get" action="https://dees.dict.cc/">
+        <div class="field has-addons">
+          <div class="control has-icons-left">
+            <span class="select">
+              <select v-model="selected_search_engine">
+                <option>google</option>
+                <option>duckduckgo</option>
+                <option>dict.cc</option>
+                <option>dees.cc</option>
+                <option>google images</option>
+                <option>wikipedia.de</option>
+                <option>wikipedia.com</option>
+                <option>Metal Archive</option>
+              </select>
+            </span>
+            <span class="icon is-small is-left">
+              <i class="fab fa-searchengin"></i>
+            </span>
+          </div>
+          <div class="control is-expanded">
+            <input class="input" type="text" placeholder="search..." name="s" maxlength="255">
+          </div>
+          <div class="control">
+            <button type="submit" class="button is-info"><i class="fas fa-search"></i></button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </template>
+
+
   </section>
 </template>
 
 <script>
 export default {
   name: "searchbar",
+
+  data() {
+    return {
+      selected_search_engine: 'google'
+    }
+  }
 }
 </script>
 
