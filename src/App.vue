092999c89c239328c4059c8ad51563e7b547061e
t0biwan <tobi.klatt@web.de>
2018-04-03 14:33:37 +0200

    search query to google works

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,70 @@
 <template>
-<div id="app">
-  <!-- <hero title="In Production:">Selected search engine: {{selected_search_engine}}</hero> -->
-  <logo></logo>
+  <div id="app">
+    <!-- <hero title="In Production:">Selected search engine: {{selected_search_engine}}</hero> -->
+    <logo></logo>
 
-  <section class="section">
-    <form method="get" action="http://www.google.com/search">
-      <div class="field has-addons">
-        <div class="control has-icons-left">
-          <span class="select">
-            <select v-model="selected_search_engine">
-              <option>google</option>
-              <option>duckduckgo</option>
-              <option>dict.cc</option>
-              <option>google images</option>
-              <option>wikipedia.de</option>
-              <option>wikipedia.com</option>
-              <option>Metal Archive</option>
-            </select>
-          </span>
-          <span class="icon is-small is-left">
-            <i class="fab fa-searchengin"></i>
-          </span>
+    <section class="section">
+      <form method="get" action="http://www.google.com/search">
+        <div class="field has-addons">
+          <div class="control has-icons-left">
+            <span class="select">
+              <select v-model="selected_search_engine">
+                <option>google</option>
+                <option>duckduckgo</option>
+                <option>dict.cc</option>
+                <option>google images</option>
+                <option>wikipedia.de</option>
+                <option>wikipedia.com</option>
+                <option>Metal Archive</option>
+              </select>
+            </span>
+            <span class="icon is-small is-left">
+              <i class="fab fa-searchengin"></i>
+            </span>
+          </div>
+          <div class="control is-expanded">
+            <input class="input" type="text" placeholder="search..." name="query" maxlength="255">
+          </div>
+          <div class="control">
+            <button type="submit" class="button is-info"><i class="fas fa-search"></i></button>
+          </div>
         </div>
-        <div class="control is-expanded">
-          <input class="input" type="text" placeholder="search..." name="query" maxlength="255">
-        </div>
-        <div class="control">
-          <button type="submit" class="button is-info"><i class="fas fa-search"></i></button>
+      </form>
+    </section>
+
+    <!-- besser grid css oder flexbox ansehen und erst mal auf papier designen -->
+    <!-- <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <figure class="image is-128x128">
+              <img src="https://bulma.io/images/placeholders/128x128.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img src="https://bulma.io/images/placeholders/128x128.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img src="https://bulma.io/images/placeholders/128x128.png">
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-128x128">
+              <img src="https://bulma.io/images/placeholders/128x128.png">
+            </figure>
+          </div>
         </div>
       </div>
-    </form>
-  </section>
-
-  <!-- besser grid css oder flexbox ansehen und erst mal auf papier designen -->
-  <!-- <section class="section">
-    <div class="container">
-      <div class="columns">
-        <div class="column">
-          <figure class="image is-128x128">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
-          </figure>
-        </div>
-        <div class="column">
-          <figure class="image is-128x128">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
-          </figure>
-        </div>
-        <div class="column">
-          <figure class="image is-128x128">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
-          </figure>
-        </div>
-        <div class="column">
-          <figure class="image is-128x128">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
-          </figure>
-        </div>
+    </section> -->
+    <section class="section">
+      <div class="container">
+        <linkbox title="Google Calendar" href="https://calendar.google.com/calendar/r"></linkbox>
       </div>
-    </div>
-  </section> -->
-  <section class="section">
-    <div class="container">
-      <linkbox title="Google Calendar" href="https://calendar.google.com/calendar/r"></linkbox>
-
-
-    </div>
-
-  </section>
-</div>
+    </section>
+  </div>
 </template>
 
 <script>
