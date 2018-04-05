e361a55a2f8b041bc409eae0c6424dd30428ea3d
t0biwan <tobi.klatt@web.de>
2018-04-05 14:57:56 +0200

    added container

--- src/components/searchbar.vue
+++ src/components/searchbar.vue
@@ -1,25 +1,27 @@
 <template>
   <section class="section">
-    <form method="get" v-bind:action="this.search_engine_action">
-      <div class="field has-addons">
-        <div class="control has-icons-left">
-          <span class="select">
-            <select v-model="selected_search_engine">
-              <option v-for="(value, key) in search_engines">{{key}}</option>
-            </select>
-          </span>
-          <span class="icon is-small is-left">
-            <i class="fab fa-searchengin"></i>
-          </span>
+    <div class="container">
+      <form method="get" v-bind:action="this.search_engine_action">
+        <div class="field has-addons">
+          <div class="control has-icons-left">
+            <span class="select">
+              <select v-model="selected_search_engine">
+                <option v-for="(value, key) in search_engines">{{key}}</option>
+              </select>
+            </span>
+            <span class="icon is-small is-left">
+              <i class="fab fa-searchengin"></i>
+            </span>
+          </div>
+          <div class="control is-expanded">
+            <input class="input" type="text" placeholder="search..." v-bind:name="search_engine_input_name" maxlength="255">
+          </div>
+          <div class="control">
+            <button type="submit" class="button is-info"><i class="fas fa-search"></i></button>
+          </div>
         </div>
-        <div class="control is-expanded">
-          <input class="input" type="text" placeholder="search..." v-bind:name="search_engine_input_name" maxlength="255">
-        </div>
-        <div class="control">
-          <button type="submit" class="button is-info"><i class="fas fa-search"></i></button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
   </section>
 </template>
 
