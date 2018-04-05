f5eb869ae1fe32cbdb7ce0ddbfcbc626f57d46bd
t0biwan <tobi.klatt@web.de>
2018-04-05 14:58:40 +0200

    made layout smaler from the sides and worked on layout for links

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <logo imagepath="src\assets\logo\wave-logo-color.png"></logo>
     <searchbar></searchbar>
+
     <!-- besser grid css oder flexbox ansehen und erst mal auf papier designen -->
+
     <!-- <section class="section">
       <div class="container">
         <div class="columns">
@@ -31,9 +33,78 @@
     </section> -->
     <section class="section">
       <div class="container">
-        <linkbox title="Google Calendar" href="https://calendar.google.com/calendar/r"></linkbox>
+        <!-- <linkbox title="Google Calendar" href="https://calendar.google.com/calendar/r"></linkbox>
+        <linkbox title="Google Contacts" href="https://contacts.google.com/"></linkbox> -->
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <div class="tile is-child">
+              <article class="card">
+                <p>
+                  hallo
+                </p>
+              </article>
+            </div>
+          </div>
+          <div class="tile is-parent">
+            <div class="tile is-child">
+              <article class="card">
+                <p>
+                  hallo
+                </p>
+              </article>
+            </div>
+          </div>
+          <div class="tile is-parent">
+            <div class="tile is-child">
+              <article class="card">
+                <p>
+                  hallo
+                </p>
+              </article>
+            </div>
+          </div>
+          <div class="tile is-parent">
+            <div class="tile is-child">
+              <article class="card">
+                <p>
+                  hallo
+                </p>
+              </article>
+            </div>
+          </div>
+
+        </div>
+
+        <!-- <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+              <p class="title">One</p>
+              <p class="subtitle">Subtitle</p>
+            </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+              <p class="title">Two</p>
+              <p class="subtitle">Subtitle</p>
+            </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+              <p class="title">Three</p>
+              <p class="subtitle">Subtitle</p>
+            </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+              <p class="title">Four</p>
+              <p class="subtitle">Subtitle</p>
+            </article>
+          </div>
+        </div> -->
+
       </div>
     </section>
+
   </div>
 </template>
 
@@ -55,4 +126,12 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.abc {
+  background: blue;
+}
+
+.container {
+  width: 60%;
+}
+</style>
