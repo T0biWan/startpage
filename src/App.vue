d54e4a5e877313ac384d79926ee56f5897c08b70
t0biwan <tobi.klatt@web.de>
2018-04-06 11:16:57 +0200

    implemented grid with flexbox

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app">
+<div id="app">
+  <div class="container">
     <logo imagepath="src\assets\logo\wave-logo-color.png"></logo>
     <searchbar></searchbar>
 
@@ -31,86 +32,26 @@
         </div>
       </div>
     </section> -->
-    <section class="section">
-      <div class="container">
-        <!-- <linkbox title="Google Calendar" href="https://calendar.google.com/calendar/r"></linkbox>
+    <!-- <linkbox title="Google Calendar" href="https://calendar.google.com/calendar/r"></linkbox>
         <linkbox title="Google Contacts" href="https://contacts.google.com/"></linkbox> -->
-        <div class="tile is-ancestor">
-          <div class="tile is-parent">
-            <div class="tile is-child">
-              <article class="card">
-                <p>
-                  hallo
-                </p>
-              </article>
-            </div>
-          </div>
-          <div class="tile is-parent">
-            <div class="tile is-child">
-              <article class="card">
-                <p>
-                  hallo
-                </p>
-              </article>
-            </div>
-          </div>
-          <div class="tile is-parent">
-            <div class="tile is-child">
-              <article class="card">
-                <p>
-                  hallo
-                </p>
-              </article>
-            </div>
-          </div>
-          <div class="tile is-parent">
-            <div class="tile is-child">
-              <article class="card">
-                <p>
-                  hallo
-                </p>
-              </article>
-            </div>
-          </div>
 
-        </div>
-
-        <!-- <div class="tile is-ancestor">
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">One</p>
-              <p class="subtitle">Subtitle</p>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">Two</p>
-              <p class="subtitle">Subtitle</p>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">Three</p>
-              <p class="subtitle">Subtitle</p>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">Four</p>
-              <p class="subtitle">Subtitle</p>
-            </article>
-          </div>
-        </div> -->
-
-      </div>
-    </section>
+    <div class="links-container">
+      <div class="link">1</div>
+      <div class="link">2</div>
+      <div class="link">3</div>
+      <div class="link">4</div>
+      <div class="link">5</div>
+      <div class="link">6</div>
+      <div class="link">7</div>
+      <div class="link">8</div>
+    </div>
 
   </div>
+</div>
 </template>
 
 <script>
 import logo from "./components/logo.vue"
-import hero from "./components/hero.vue"
 import linkbox from "./components/linkbox.vue"
 import searchbar from "./components/searchbar.vue"
 
@@ -119,7 +60,6 @@ export default {
 
   components: {
     logo,
-    hero,
     linkbox,
     searchbar,
   },
@@ -127,11 +67,24 @@ export default {
 </script>
 
 <style lang="scss">
-.abc {
-  background: blue;
+@import 'node_modules/sass-material-design-colors/palette.scss';
+
+.links-container {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    margin-top: 2.2vw;
+}
+
+.link {
+    color: #5995DA;
+    background-color: $grey-400;
+    width: 11vw;
+    height: 11vw;
+    margin-bottom: 2.2vw;
 }
 
 .container {
-  width: 60%;
+    width: 50%;
 }
 </style>
