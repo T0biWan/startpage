4a61d3890fe01efe6469356c80364cd9568ca7e9
t0biwan <tobi.klatt@web.de>
2018-04-06 14:22:07 +0200

    build new hyperlink component

---
+++ src/components/hyperlinks.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="links-container">
+    <hyperlink title="Calendar" href="https://calendar.google.com/calendar/r" image="https://source.unsplash.com/zni0zgb3bkQ"></hyperlink>
+    <hyperlink title="Contacts" href="https://contacts.google.com/" image="https://source.unsplash.com/ZGyaL6QL7VE"></hyperlink>
+    <hyperlink title="Calendar" href="https://calendar.google.com/calendar/r"></hyperlink>
+    <hyperlink title="Calendar" href="https://calendar.google.com/calendar/r" image="https://source.unsplash.com/ZGyaL6QL7VE"></hyperlink>
+    <hyperlink title="Calendar" href="https://calendar.google.com/calendar/r" image="https://picsum.photos/200/200/?random"></hyperlink>
+    <hyperlink title="Calendar" href="https://calendar.google.com/calendar/r" image="https://picsum.photos/200/200/?random"></hyperlink>
+    <hyperlink title="Calendar" href="https://calendar.google.com/calendar/r" image="https://picsum.photos/200/200/?random"></hyperlink>
+    <hyperlink title="Calendar" href="https://calendar.google.com/calendar/r" image="https://picsum.photos/200/200/?random"></hyperlink>
+  </div>
+</template>
+
+<script>
+import hyperlink from "./hyperlink.vue"
+
+export default {
+  name: "hyperlinks",
+
+  components: {
+    hyperlink,
+  },
+}
+</script>
+
+<style lang="scss">
+.links-container {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    margin-top: 2.2vw;
+}
+</style>
