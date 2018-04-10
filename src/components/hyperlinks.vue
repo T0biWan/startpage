83eaf93dba26784ac33dc3498eacd7df75a3c531
t0biwan <tobi.klatt@web.de>
2018-04-10 14:34:19 +0200

    added new images

--- src/components/hyperlinks.vue
+++ src/components/hyperlinks.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="links-container">
-    <hyperlink title="Calendar" href="https://calendar.google.com/calendar/r" image="https://source.unsplash.com/zni0zgb3bkQ"></hyperlink>
-    <hyperlink title="Contacts" href="https://contacts.google.com/" image="https://source.unsplash.com/ZGyaL6QL7VE"></hyperlink>
-    <hyperlink title="YouTube" href="https://www.youtube.com" image="https://source.unsplash.com/NmGzVG5Wsg8"></hyperlink>
-    <hyperlink title="imgur" href="https://imgur.com/" image="https://source.unsplash.com/Fo5dTm6ID1Y"></hyperlink>
+    <hyperlink title="Calendar" href="https://calendar.google.com/calendar/r" image="src/assets/img/hope-house-press-127593-unsplash.jpg"></hyperlink>
+    <hyperlink title="Contacts" href="https://contacts.google.com/" image="src/assets/img/nik-macmillan-309702-unsplash.jpg"></hyperlink>
+    <hyperlink title="YouTube" href="https://www.youtube.com" image="src/assets/img/ariel-domenden-583207-unsplash.jpg"></hyperlink>
+    <hyperlink title="imgur" href="https://imgur.com/" image="src/assets/img/brooke-lark-609907-unsplash.jpg"></hyperlink>
 
-    <hyperlink title="GitHub" href="https://github.com/" image="https://source.unsplash.com/fPkvU7RDmCo"></hyperlink>
-    <hyperlink title="whatthecommit" href="http://whatthecommit.com/" image="https://source.unsplash.com/Skf7HxARcoc"></hyperlink>
-    <hyperlink title="RegEx" href="https://regex101.com/" image="https://source.unsplash.com/466ENaLuhLY"></hyperlink>
-    <hyperlink title="JSON" href="https://jsoneditoronline.org/" image="https://source.unsplash.com/feXpdV001o4"></hyperlink>
+    <hyperlink title="Disroot" href="https://apps.disroot.org/" image="src/assets/img/oscar-nord-588057-unsplash.jpg"></hyperlink>
+    <hyperlink title="whatthecommit" href="http://whatthecommit.com/" image="src/assets/img/simon-abrams-286276-unsplash.jpg"></hyperlink>
+    <hyperlink title="RegEx" href="https://regex101.com/" image="src/assets/img/chris-ried-532181-unsplash.jpg"></hyperlink>
+    <hyperlink title="JSON" href="https://jsoneditoronline.org/" image="src/assets/img/andrew-neel-218073-unsplash.jpg"></hyperlink>
+
   </div>
 </template>
 
