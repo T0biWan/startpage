16bcce54273392830446ec5847c726eb2cd5f154
Tobias Klatt <tobi.klatt@web.de>
2020-06-07 13:43:45 +0200

    build containers for links and centered everything

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,13 @@
       <div class="background-gradient h-screen"></div>
       <div class="background-image"></div>
       <div class="foreground text-white">
-        <div v-for="link in links" :key="link.title" class="bg-link">
-          <a :href="link.link">{{ link.title }}</a>
+        <div class="mt-48 flex justify-center mx-32">
+          <div class="flex flex-wrap">
+            <div v-for="category in categorys()" :key="category" class="w-56 mx-4 mb-6">
+              <h1 class="bg-category text-xl font-semibold pl-2 pb-2 pt-1">{{ category }}</h1>
+              <div v-for="link in linksByCategory(category)" :key="link.title" class="bg-link pl-2 pb-2 text-lg"><a :href="link.link">{{ link.title }}</a></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -19,30 +24,37 @@ export default {
   data () {
     return {
       links: [
-        { title: 'youtube', link: 'https://www.youtube.com/', category: '', icon: '' },
-        { title: 'udemy', link: 'https://www.udemy.com/home/my-courses/learning/', category: '', icon: '' },
-        { title: 'imgur', link: 'https://imgur.com/t/meme%20dump', category: '', icon: '' },
-        { title: 'picdumbs', link: 'https://www.picdumps.com/', category: '', icon: '' },
-        { title: 'netflix', link: 'https://www.netflix.com/browse', category: '', icon: '' },
-        { title: 'prime', link: 'https://www.amazon.de/Prime-Video/', category: '', icon: '' },
-        { title: 'calender', link: 'https://calendar.google.com/calendar/r', category: '', icon: '' },
-        { title: 'contacts', link: 'https://contacts.google.com/', category: '', icon: '' },
-        { title: 'maps', link: 'https://www.google.com/maps/', category: '', icon: '' },
-        { title: 'maps timeline', link: 'https://www.google.com/maps/timeline', category: '', icon: '' },
-        { title: 'notes', link: 'https://github.com/T0biWan/notes-cms', category: '', icon: '' },
-        { title: 'disroot', link: 'https://apps.disroot.org/', category: '', icon: '' },
-        { title: 'tasks', link: 'https://trello.com/b/SKPdrjlw/task-force', category: '', icon: '' }
+        { title: 'tasks', link: 'https://trello.com/b/SKPdrjlw/task-force', category: 'office', icon: '' },
+        { title: 'notes', link: 'https://github.com/T0biWan/notes-cms', category: 'office', icon: '' },
+        { title: 'calender', link: 'https://calendar.google.com/calendar/r', category: 'office', icon: '' },
+        { title: 'contacts', link: 'https://contacts.google.com/', category: 'office', icon: '' },
+        { title: 'disroot', link: 'https://apps.disroot.org/', category: 'office', icon: '' },
+        { title: 'udemy', link: 'https://www.udemy.com/home/my-courses/learning/', category: 'education', icon: '' },
+        { title: 'maps', link: 'https://www.google.com/maps/', category: 'service', icon: '' },
+        { title: 'maps timeline', link: 'https://www.google.com/maps/timeline', category: 'service', icon: '' },
+        { title: 'youtube', link: 'https://www.youtube.com/', category: 'entertainment', icon: '' },
+        { title: 'netflix', link: 'https://www.netflix.com/browse', category: 'entertainment', icon: '' },
+        { title: 'prime', link: 'https://www.amazon.de/gp/video/storefront/', category: 'entertainment', icon: '' },
+        { title: 'imgur', link: 'https://imgur.com/t/meme%20dump', category: 'entertainment', icon: '' },
+        { title: 'picdumbs', link: 'https://www.picdumps.com/', category: 'entertainment', icon: '' }
       ]
     }
+  },
+
+  methods: {
+    categorys () { return [...new Set(this.links.map(link => link.category))] },
+    linksByCategory (category) { return this.links.filter(link => link.category === category) }
   }
 }
 </script>
 
 <style>
+.bg-category {
+  background-color: rgba(255, 255, 255, 0.18);
+}
+
 .bg-link {
-  /* background-color: rgb(18, 4, 40); */
-  /* background-image: linear-gradient(to right, rgb(143, 32, 98), rgb(9, 2, 53)); */
-  /* background-color: white; */
+  background-color: rgba(255, 255, 255, 0.05);
 }
 
 .container_row{
